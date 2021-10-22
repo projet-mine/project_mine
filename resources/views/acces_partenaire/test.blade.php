b272740bf4ccac292c1eac4eaa183f1d5a6748e5
lailahamza <56793079+lailahamza@users.noreply.github.com>
2021-10-23 00:38:35 +0100

    Update Password

--- resources/views/acces_partenaire/test.blade.php
+++ resources/views/acces_partenaire/test.blade.php
@@ -1,11 +1,12 @@
 <!DOCTYPE html>
-<html lang="fr">
+<html lang="{{str_replace('_', '-', app()->getLocale())}}">
     <head>
         <title>Acces Patenaire - MIC</title>
         <meta charset="utf-8">
         <meta content="width=device-width, initial-scale=1.0" name="viewport">
         <meta content="" name="description">
         <meta content="" name="keywords">
+        <script src="https://kit.fontawesome.com/5fdfb057c8.js" crossorigin="anonymous"></script>
         @include('components.css')
     </head>
     <body>
@@ -15,6 +16,8 @@
         <style type="text/css">
             body{
                 background: #f7f7ff;
+                /* font-family: calibri !important; */
+                /* font-family: Helvetica, Arial, sans-serif !important; */
             }
             .card {
                 position: relative;
@@ -36,12 +39,12 @@
         <div class="container mt-5">
             <div class="main-body">
                 <div class="row">
-                    <div class="col-lg-4">
+                    <div class="col-lg-3">
                         <div class="card">
-                            <div class="card-body py-5">
+                            <div class="card-body py-3">
                                 <div class="flex-column align-items-center text-center">
                                     <img src="img/profile.png" width="120px" height="120px" alt="Photo de profil" class="rounded-circle p-1 bg-transparent" width="110">
-                                    <div class="mt-3">
+                                    <div class="mt-2">
                                         <h4 class="text-capitalize">{{Auth::user()->last_name}} {{Auth::user()->first_name}}</h4>
                                         <p class="text-secondary mb-1 text-capitalize">{{Auth::user()->institut}}</p>
 
@@ -50,7 +53,7 @@
                             </div>
                         </div>
                     </div>
-                    <div class="col-lg-8">
+                    <div class="col-lg-9">
                         <div class="card">
                             <div class="card-body">
                                 <h5 class="mb-5"><i class="fas fa-user-edit fs-4 pe-3"></i>Mettre à jour les informations personnelles</h5>
@@ -73,7 +76,7 @@
                                     </div> --}}
                                     <div class="row mb-3">
                                         <div class="col-sm-3">
-                                            <h6 class="mb-0">Email</h6>
+                                            <h6 class="mb-0">Adresse e-mail</h6>
                                         </div>
                                         <div class="col-sm-9 text-secondary">
                                             <input type="text" value="{{Auth::user()->email}}" class="rounded-0 form-control" name="email">
@@ -89,67 +92,74 @@
                                     </div> --}}
                                     <div class="row">
                                         <div class="mr-0" style="max-width: max-content; margin-left: auto;">
-                                            <input type="submit" class="rounded-1 btn  btn-success bg-base border-0" value="Sauvegarder les modifications">
+                                            <button type="submit" class="mt-3 mb-3 rounded-1 btn btn-success bg-base border-0 ">Enregistrer les modifications</button>
                                         </div>
                                     </div>
                                 </form>
                             </div>
                         </div>
-                        <div class="card">
-                            <div class="card-body">
-                                <h5 class="pb-5" ><i class="fas fa-user-lock fs-4 pe-3"></i>Changer mon mot de passe</h5>
-                                <form action="{{route('updatePassword.update' , Auth::user()->id)}}" method="post">
-                                    @csrf
-                                    @method('put')
-                                    @if ($message = Session::get('successPassword'))
-                                    <div class="alert alert-success">
-                                        <strong>{{ $message }}</strong>
-                                    </div>
-                                    @endif
-                                    @if ($message = Session::get('FalsePassword'))
-                                    <div class="alert">
-                                        <strong class="text-danger">{{ $message }}</strong>
-                                    </div>
-                                    @endif
+                    </div>
+                    <div class="col-lg">
+                        <div class="col-lg">
+                            <div class="card">
+                                <div class="card-body">
+                                    <h5 class="pb-5" ><i class="fas fa-user-lock fs-4 pe-3"></i>Changer le mot de passe</h5>
+                                    <form action="{{route('updatePassword.update' , Auth::user()->id)}}" method="post">
+                                        @csrf
+                                        @method('put')
+                                        @if ($message = Session::get('successPassword'))
+                                        <div class="alert alert-success">
+                                            <strong>{{ $message }}</strong>
+                                        </div>
+                                        @endif
+                                        @if ($message = Session::get('FalsePassword'))
+                                        <div class="alert">
+                                            <strong class="text-danger">{{ $message }}</strong>
+                                        </div>
+                                        @endif
 
-                                    <div class="row mb-3">
-                                        <div class="col-sm-3">
-                                            <h6 class="mb-0">Mot de passe actuel</h6>
+                                        <div class="row mb-3">
+                                            <div class="col-sm-3">
+                                                <h6 class="mb-0">Mot de passe actuel</h6>
+                                            </div>
+                                            <div class="col-sm-9 text-secondary">
+                                                <input name="password" type="password" class="rounded-0 form-control" id="exampleInputPassword1" required>
+                                            </div>
                                         </div>
-                                        <div class="col-sm-9 text-secondary">
-                                            <input name="password" type="password" class="rounded-0 form-control" id="exampleInputPassword1" required>
+                                        @if ($message = Session::get('PasswordsDifferent'))
+                                        <div class="alert">
+                                            <strong class="text-danger">{{ $message }}</strong>
                                         </div>
-                                    </div>
-                                    @if ($message = Session::get('PasswordsDifferent'))
-                                    <div class="alert">
-                                        <strong class="text-danger">{{ $message }}</strong>
-                                    </div>
-                                    @endif
-                                    @if ($message = Session::get('ShortPassword'))
-                                    <div class="alert">
-                                        <strong class="text-danger">{{ $message }}</strong>
-                                    </div>
-                                    @endif
-                                    <div class="row mb-3">
-                                        <div class="col-sm-3">
-                                            <h6 class="mb-0">Nouveau mot de passe</h6>
+                                        @endif
+                                        @if ($message = Session::get('ShortPassword'))
+                                        <div class="alert">
+                                            <strong class="text-danger">{{ $message }}</strong>
                                         </div>
-                                        <div class="col-sm-9 text-secondary">
-                                            <input name="NewPassword" type="password" class="rounded-0 form-control" id="exampleInputPassword1" required>
+                                        @endif
+                                        <div class="row mb-3">
+                                            <div class="col-sm-3">
+                                                <h6 class="mb-0">Nouveau mot de passe</h6>
+                                            </div>
+                                            <div class="col-sm-9 text-secondary">
+                                                <input name="NewPassword" type="password" class="rounded-0 form-control" id="exampleInputPassword1" required>
+                                            </div>
                                         </div>
-                                    </div>
-                                    <div class="row mb-3">
-                                        <div class="col-sm-3">
-                                            <h6 class="mb-0">Confirmer votre mot de passe</h6>
+                                        <div class="row mb-3">
+                                            <div class="col-sm-3">
+                                                <h6 class="mb-0 fst-normal">Confirmer le mot de passe</h6>
+                                            </div>
+                                            <div class="col-sm-9 text-secondary">
+                                                <input  name="ConfirmNewPassword" type="password" class="rounded-0 form-control" id="exampleInputPassword2" required>
+                                            </div>
                                         </div>
-                                        <div class="col-sm-9 text-secondary">
-                                            <input  name="ConfirmNewPassword" type="password" class="rounded-0 form-control" id="exampleInputPassword2" required>
+                                        <div class="row">
+                                            <a href="" class="mt-3 text-end">Mot de passe oublié?</a>
                                         </div>
-                                    </div>
-                                    <div class="mr-0" style="max-width: max-content; margin-left: auto;">
-                                        <button type="submit" class="mt-3 mb-3 rounded-1 btn btn-success bg-base border-0 ">Modifier le mot de passe<i class="fas fa-check ps-2"></i></button>
-                                    </div>
-                                </form>
+                                        <div class="mr-0" style="max-width: max-content; margin-left: auto;">
+                                            <button type="submit" class="mt-3 mb-3 rounded-1 btn btn-success border-0 ">Enregistrer les modifications</button>
+                                        </div>
+                                    </form>
+                                </div>
                             </div>
                         </div>
                     </div>
